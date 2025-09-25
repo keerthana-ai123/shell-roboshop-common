de557ca8a714e6455f8b0b9d6dd5e75d57d822f8
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-09-25 21:07:18 +0530

    variables

--- common.sh
+++ common.sh
@@ -9,6 +9,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
+SCRIPT_DIR=$PWD
 MONGODB_HOST=172.31.23.52
 
 USERID=$(id -u)
