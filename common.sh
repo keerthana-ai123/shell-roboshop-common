e5860830f74cb4e17a94911a8cdfe2bb07496f02
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-09-25 21:01:59 +0530

    variables

--- common.sh
+++ common.sh
@@ -9,6 +9,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
+MONGODB_HOST=172.31.23.52
 
 USERID=$(id -u)
 
@@ -30,6 +31,51 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 else 
     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
 fi
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disabling NodeJS"
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "Enabling NodeJS 20"
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Installing NodeJS"
+
+    npm install &>>$LOG_FILE
+    VALIDATE $? "Install dependencies"
+}
+
+
+app_setup(){
+    mkdir -p /app 
+    VALIDATE $? "Creating app directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "Downloading $app_name application"
+
+    cd /app 
+    VALIDATE $? "Changing to app directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing existing code"
+
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzip $app_name"
+}
+
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service  /etc/systemd/system/$app_name.service
+    VALIDATE $? "Copy systemctl service"
+
+    systemctl daemon-reload
+    systemctl enable $app_name &>>$LOG_FILE
+    VALIDATE $? "Enable $app_name"
+}
+
+app_restart(){
+    systemctl restart $app_name
+    VALIDATE $? "Restarted $app_name"
 }
 
 print_total_time(){
